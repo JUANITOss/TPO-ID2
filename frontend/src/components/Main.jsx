94064da8184295679873b2756fe6919ee0498309
JUANITOss <113447766+JUANITOss@users.noreply.github.com>
2024-06-13 05:47:40 -0300

    Logout funciona

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,71 +1,66 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import api from '../api';
 
 const Main = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await api.post('/login/logout');
+      
+      if (response.status === 200) {
+        // Limpiar la sesión localmente y redirigir a /
+        navigate('/');
+      } else {
+        // Manejar errores si es necesario
+        console.error('Logout failed');
+      }
+    } catch (error) {
+      console.error('Error during logout:', error);
+    }
+  };
+
   return (
     <section className="flex justify-center h-screen bg-neutral-800">
-      {/* HEADER */}
-            <div className="self-start mb-20 container grid gap-6 md:gap-8 px-4 md:px-6 bg-neutral-800">
-                <div 
-                    className="flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-8"
-                    style={{ marginTop: '75PX' }}
-                    >
-                    <h2 className="text-2xl font-bold tracking-tight text-white">
-                        Explore Our Products
-                    </h2>
-                        <p className="text-gray-400 max-w-md">
-                            Browse through our wide range of products and find the perfect items for your needs
-                        </p>
-                        <div className="flex items-center gap-2 ml-auto shrink-0">
-                            <Link to="/info-cart">
-                                <button 
-                                    className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">
-                                        View Cart
-                                </button>
-                            </Link>
-                            <Link to="/">
-                                <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">
-                                    Logout
-                                </button>
-                            </Link>
-                        </div>
-                </div>
-      {/* body */}
-      <div className='flex items-center bg-neutral-800 justify-center'
-            style={{paddingTop: '100px' }}
+      <div className="self-start mb-20 container grid gap-6 md:gap-8 px-4 md:px-6 bg-neutral-800">
+        <div className="flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-8" style={{ marginTop: '75px' }}>
+          <h2 className="text-2xl font-bold tracking-tight text-white">Explore Our Products</h2>
+          <p className="text-gray-400 max-w-md">
+            Browse through our wide range of products and find the perfect items for your needs
+          </p>
+          <div className="flex items-center gap-2 ml-auto shrink-0">
+            <Link to="/info-cart">
+              <button className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">
+                View Cart
+              </button>
+            </Link>
+            <button
+              onClick={handleLogout}
+              className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white"
             >
-       <nav>
-        <ul className="nav-list grid grid-rows-5 gap-4">
-          <li>
-            <Link to="/add-product" className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">Crear Producto</Link>
-          </li>
-          <li>
-            <Link to="/list-product" className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">Ver Productos</Link>
-          </li>
-          <li>
-            <Link to="/update-product" className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">Actualizar Productos</Link>
-          </li>
-          <li>
-            <Link to="/list-order" className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">Ver Facturas</Link>
-          </li>
-          <li>
-            <Link to="/create-bill" className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">Create bill</Link>
-          </li>
-          <li>
-            <Link to="/list-bills" className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">Ver Bills</Link>
-          </li>
-          <li>
-            <Link to="/update-bill" className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">Actualizar Bills</Link>
-          </li>
-          
-          
-        </ul>
-      </nav> 
-     </div>
-    
-  </div>
-  </section>
+              Logout
+            </button>
+          </div>
+        </div>
 
+        <div className="flex items-center bg-neutral-800 justify-center" style={{ paddingTop: '100px' }}>
+          <nav>
+            <ul className="nav-list grid grid-rows-5 gap-4">
+              <li>
+                <Link
+                  to="/add-product"
+                  className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white"
+                >
+                  Create Product
+                </Link>
+              </li>
+              {/* Otras opciones de navegación */}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- routes/login.js
+++ routes/login.js
@@ -9,18 +9,27 @@ router.post('/', async (req, res) => {
   if (!username || !password) {
     return res.status(400).json({ error: 'Username and password are required' });
   }
+
   try {
     const storedUser = await req.redisClient.HGET('users', username);
+
     if (!storedUser) {
       return res.status(400).json({ error: 'Invalid username or password' });
     }
 
-    const user = JSON.parse(storedUser);
+    let user = JSON.parse(storedUser);
 
     // Comparar la contraseña hasheada almacenada con la contraseña proporcionada
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (passwordMatch) {
+      // Actualizar la fecha de ingreso en el objeto del usuario
+      const currentDate = new Date().toISOString();
+      user.loginTime = currentDate;
+
+      // Guardar el usuario actualizado en Redis
+      await req.redisClient.HSET('users', username, JSON.stringify(user));
+
       // Acceder a la sesión desde otras rutas
       req.session.userId = username;
 
@@ -47,6 +56,44 @@ router.post('/', async (req, res) => {
   } catch(err) {
     console.error('Error logging in:', err);
     return res.status(500).json({ error: 'Error logging in' });
+  }
+});
+
+router.post('/logout', async (req, res) => {
+  try {
+    const username = req.session.userId;
+
+    if (!username) {
+      return res.status(400).json({ error: 'User is not logged in' });
+    }
+
+    // Obtener el usuario desde Redis
+    const storedUser = await req.redisClient.HGET('users', username);
+
+    if (!storedUser) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    let user = JSON.parse(storedUser);
+
+    // Agregar la fecha de cierre de sesión al usuario
+    const logoutTime = new Date().toISOString();
+    user.logoutTime = logoutTime;
+
+    // Actualizar el usuario en Redis con la fecha de cierre de sesión
+    await req.redisClient.HSET('users', username, JSON.stringify(user));
+
+    // Limpiar la sesión en Express
+    req.session.destroy((err) => {
+      if (err) {
+        console.error('Error destroying session:', err);
+        return res.status(500).json({ error: 'Failed to logout' });
+      }
+      return res.status(200).json({ message: 'Logout successful' });
+    });
+  } catch (err) {
+    console.error('Error logging out:', err);
+    return res.status(500).json({ error: 'Error logging out' });
   }
 });
 
