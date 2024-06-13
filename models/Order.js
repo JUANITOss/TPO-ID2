fe28d55523b489f1dcc23b94879ce77f7c23ea71
JUANITOss <113447766+JUANITOss@users.noreply.github.com>
2024-06-12 22:13:23 -0300

    Model Order cambiado

--- models/Order.js
+++ models/Order.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
   userId: String,
+  nombreResponsable: String,
+  apellidoResponsable: String,
+  recargo: 21, // IVA
   productos: [
     {
-      productoId: String,
       nombreProducto: String,
-      cantidad: Number,
-      precioUnitario: Number,
-      descuento: Number,
-      impuesto: Number
+      total: Number, // cantidad * precioUnitario
+      descuento: Number, // Setteado a mano para los propositos de la entrega
+      impuesto: Number, // Setteado a mano para los propositos de la entrega, agregar el recargo
     }
   ],
   fechaPedido: String,
