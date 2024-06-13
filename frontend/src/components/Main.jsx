4f04582166b7cd5856b4bfd2af20ebe919d99f4b
lui <ingrassialuispedro@gmail.com>
2024-06-13 06:59:31 -0300

    estoy re quemado matenme

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -46,7 +46,7 @@ const Main = () => {
 
         <div className="flex items-center bg-neutral-800 justify-center" style={{ paddingTop: '100px' }}>
           <nav>
-            <ul className="nav-list grid grid-rows-5 gap-4">
+            <ul className="nav-list grid gap-2">
               <li>
                 <Link
                   to="/add-product"
@@ -54,6 +54,32 @@ const Main = () => {
                 >
                   Create Product
                 </Link>
+              </li>
+              <li>
+                <Link
+                  to="/list-product"
+                  className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white"
+                >
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/update-product"
+                  className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white"
+                >
+                  Modify Product
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/list-order"
+                  className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white"
+                >
+                  My Order
+                </Link>
+                <li>
+              </li>
               </li>
               {/* Otras opciones de navegación */}
             </ul>
