ff019381ebfb344a3aab724c38baa595ab61b9fa
lui <ingrassialuispedro@gmail.com>
2024-06-19 18:00:26 -0300

    df

--- frontend/src/components/bill/BillList.jsx
+++ frontend/src/components/bill/BillList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../api';
+import { Link } from 'react-router-dom';
 
 const BillList = () => {
   const [bills, setBills] = useState([]);
@@ -28,43 +29,43 @@ const BillList = () => {
   };
 
   return (
-    <div class="flex flex-col gap-6 p-6 md:p-8 bg-neutral-800">
-  <div class="flex items-center justify-between">
-    <h1 class="text-white text-2xl font-bold">Bill</h1>
+    <div className="flex flex-col gap-6 p-6 md:p-8 bg-neutral-800">
+  <div className="flex items-center justify-between">
+    <h1 className="text-white text-2xl font-bold">Bill</h1>
     <div className="flex items-center justify-self-end space-x-4">
-      <a href="/Main">
+      <Link to="/Main">
         <button 
           className="nav-link inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 bg-white text-black hover:bg-gray-700 hover:text-white">
           Home
         </button>
-      </a>
+      </Link>
     </div>
   </div>
 
-  <div class="overflow-auto h-screen bg-neutral-800">
-    <div class="relative w-full overflow-auto">
+  <div className="overflow-auto h-screen bg-neutral-800">
+    <div className="relative w-full overflow-auto">
     {bills.length === 0 ? (
   <p>No hay facturas disponibles.</p>
 ) : (
   <table className='w-full caption-bottom text-sm'>
-    <thead class="[&amp;_tr]:border-b">
-      <tr class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-        <th class="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">OrderId</th>
-        <th class="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Usuario ID</th>
-        <th class="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Productos</th>
-        <th class="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Total</th>
-        <th class="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Fecha Factura</th>
-        <th class="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Acción</th>
+    <thead className="[&amp;_tr]:border-b">
+      <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+        <th className="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">OrderId</th>
+        <th className="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Usuario ID</th>
+        <th className="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Productos</th>
+        <th className="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Total</th>
+        <th className="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Fecha Factura</th>
+        <th className="text-white h-12 px-4 text-left align-middle font-bold text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">Acción</th>
       </tr>
     </thead>
     <tbody>
       {bills.map((bill) => (
-        <tr key={bill._id} class="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-          <td class="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.orderId}</td>
-          <td class="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.userId}</td>
-          <td class="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.productos}</td>
-          <td class="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.total}</td>
-          <td class="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.fechaFactura}</td>
+        <tr key={bill._id} className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
+          <td className="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.orderId}</td>
+          <td className="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.userId}</td>
+          <td className="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.productos}</td>
+          <td className="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.total}</td>
+          <td className="text-white p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-bold">{bill.fechaFactura}</td>
           <td className="">
           <button
               className="inline-block p-3 text-white hover:bg-red-500 focus:relative"
@@ -87,7 +88,7 @@ const BillList = () => {
               </svg>
             </button>
             <button
-              class="inline-block p-3 text-white hover:bg-green-500 focus:relative"
+              className="inline-block p-3 text-white hover:bg-green-500 focus:relative"
               title="Proceed to Pay"
               //onClick={agregar onClick de pagar aca}
             >
@@ -97,7 +98,7 @@ const BillList = () => {
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                class="h-4 w-4"
+                className="h-4 w-4"
               >
                 <path
                   stroke-linecap="round"
